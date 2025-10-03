a38c712c60e39dc9f4d2a317c8fdc2205adf7dbe
abdulhanjra <abdulhanjra@hotmail.com>
2025-10-03 20:28:48 +0500

    update: user avatar updated as an atom

--- src/app/atoms/app-user-avatar/app-user-avatar.component.ts
+++ src/app/atoms/app-user-avatar/app-user-avatar.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-avatar',
   standalone: true,
+  imports: [NgIf],
   templateUrl: './app-user-avatar.component.html',
   styleUrls: ['./app-user-avatar.component.scss']
 })
-export class AppUserAvatarComponent {}
+export class AppUserAvatarComponent {
+  @Input() fullName: string = '';
+  @Input() src?: string | null;
+
+  get initials(): string {
+    const parts = this.fullName.trim().split(/\s+/);
+    if (parts.length === 1) return parts[0][0]?.toUpperCase() ?? '';
+    return (parts[0][0] + parts[1][0]).toUpperCase();
+  }
+}
