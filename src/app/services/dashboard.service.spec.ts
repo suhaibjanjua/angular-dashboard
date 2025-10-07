0e203a57116a1f837483592745e3cb922b31260b
Suhaib Janjua <suhaib.janjua@gmail.com>
2025-10-08 03:30:04 +0500

    fix: add UserService mock to DashboardService test to prevent dependency errors

--- src/app/services/dashboard.service.spec.ts
+++ src/app/services/dashboard.service.spec.ts
@@ -1,15 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 import { DashboardService } from './dashboard.service';
+import { UserService } from './user.service';
 import { TimeRange } from '../models/widget.models';
 
 describe('DashboardService', () => {
   let service: DashboardService;
 
+  const mockUserService = {
+    getUser: jasmine.createSpy('getUser').and.returnValue(of(null)),
+    getUsers: jasmine.createSpy('getUsers').and.returnValue(of([])),
+    createUser: jasmine.createSpy('createUser').and.returnValue(of(null)),
+    updateUser: jasmine.createSpy('updateUser').and.returnValue(of(null)),
+    deleteUser: jasmine.createSpy('deleteUser').and.returnValue(of(false))
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [DashboardService]
+      providers: [
+        DashboardService,
+        { provide: UserService, useValue: mockUserService }
+      ]
     });
     service = TestBed.inject(DashboardService);
   });
