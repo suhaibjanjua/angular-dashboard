c68e0e6b66498c4897ca763d04c4d666090daea0
abdulhanjra <abdulhanjra@hotmail.com>
2025-10-09 13:34:46 +0500

    feat: making buttons as angular material buttons as a reuable component

--- src/app/atoms/app-button/app-button.component.ts
+++ src/app/atoms/app-button/app-button.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-button',
   standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app-button.component.html',
   styleUrls: ['./app-button.component.scss']
 })
 export class AppButtonComponent {
-  @Input() label: string = '';
-  @Output() buttonClick = new EventEmitter<void>();
+  @Input() label!: string;
+  @Input() icon?: string;
+  @Input() color: 'primary' | 'accent' | 'warn' | undefined = 'primary';
+  @Input() type: 'button' | 'submit' = 'button';
+  @Input() variant: 'flat' | 'raised' | 'stroked' | 'basic' = 'flat';
+  @Input() disabled = false;
+  @Input() loading = false;
 }
